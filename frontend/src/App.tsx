d28e305a6f3491a8d6fa48d2e02c3256684f7425
Waliston Euripedes <goneisgone@hotmail.com>
2022-01-11 22:55:02 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,23 @@
-import Navbar from "./componets/navbar";
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form/index';
+import Navbar from 'componets/navbar/index';
 
 function App() {
   return (
-    < Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+function Form() {
+  
+  return (
+    <h1>Pagina de Formalario</h1>
+  );
+}
+
+export default Form;
