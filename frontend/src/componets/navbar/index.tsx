295662f895d5dfd0aa50fa5c6def7dd8c39d91a4
Waliston Euripedes <goneisgone@hotmail.com>
2022-01-12 21:50:20 -0300

    Movie Card

--- frontend/src/componets/navbar/index.tsx
+++ frontend/src/componets/navbar/index.tsx
@@ -9,7 +9,7 @@ function Navbar() {
     <nav className='container'>
       <div className='dsmovie-nav-content'>
         <h1>DSMovie</h1>
-        <a href="https://github.com/bkt-tonton">
+        <a href="https://github.com/bkt-tonton" target={'_blank'}>
           <div className='dsmovie-contact-container'>
             < GithubIcon />
             <p className='dsmovie-contact-link'>/devsuperior</p>
