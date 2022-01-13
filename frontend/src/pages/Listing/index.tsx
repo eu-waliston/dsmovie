d42ac01128d77b5637195dda8dbad9f87ce438e6
Waliston Euripedes <goneisgone@hotmail.com>
2022-01-12 21:36:16 -0300

    Movie Card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,43 @@
+import MovieCard from "componets/MovieCard";
 import Pagination from "componets/Pagination";
 
 function Listing() {
-  
+
   return (
-    < Pagination/>
+
+    <>
+      < Pagination />
+
+      <div className="container">
+        <div className="row">
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            < MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            < MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            < MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            < MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3">
+            < MovieCard />
+          </div>
+
+
+        </div>
+
+      </div>
+
+    </>
+
   );
 }
 
