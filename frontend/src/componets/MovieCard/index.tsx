9ec3ec0c95f9618b2ded2dc4652d3240a94410b5
Waliston Euripedes <goneisgone@hotmail.com>
2022-01-16 08:05:26 -0300

    Props

--- frontend/src/componets/MovieCard/index.tsx
+++ frontend/src/componets/MovieCard/index.tsx
@@ -1,16 +1,15 @@
 import MovieScore from "componets/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from 'types/movie';
+
+type Props = {
+  movie: Movie;
+}
 
 
-function MovieCard() {
+function MovieCard( { movie } : Props ) {
 
-  const movie = {
-    id: 1,
-    image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5
-  };
+
 
   return (
     <div>
